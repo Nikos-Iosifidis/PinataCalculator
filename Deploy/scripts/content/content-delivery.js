9a3e56118ad3ae62598cef14fc9cc7304d719ba2
nikos iosifidis <nikos_ios@outlook.com>
2022-12-14 23:59:31 +0200

    2.4.1 - Adjustments for new UI

--- Deploy/scripts/content/content-delivery.js
+++ Deploy/scripts/content/content-delivery.js
@@ -7,80 +7,11 @@ chrome.runtime.onMessage.addListener(
 );
 
 function parseCartAndSend() {
-    var cartDiscount = $('#component-cart .cart-charge-notice-amount');
-    var cartTotal = $('#component-cart .cart-total-price');
-    var cartProductList = $('#component-cart .cart-body .cart-product-list .cart-product-list-item');
-    var cartExtras = $('#component-cart .cart-charge-notice').not(':contains("περιβαλλοντικό τέλος")').find('.cart-charge-notice-amount');
-
-    var data = parseCart(cartDiscount, cartTotal, cartExtras, cartProductList);
-    sendDataToExtension('preview_order_data_efood', data.data);
+    var shopId = localStorage.getItem(localStorageShopId);
+    var localStorKey = `cart_new__${shopId}`;
+    var cart = JSON.parse(localStorage.getItem(localStorKey)) || {};
+    var parsedCart = parseCart(cart);
+    sendDataToExtension('preview_order_data_efood', parsedCart);
 }
 
-function parseCart(cartDiscount, cartTotal, cartExtras, cartProductList) {
-    var discount = 0;
-    var total = 0;
-    var products = [];
-    var extras = 0;
-
-    if (cartDiscount.length !== 0) {
-        $.each(cartDiscount, function (i, value) {
-            var parsed = parseNumber($(value).text());
-            if (parsed < 0) {
-                discount += parsed;
-            }
-        });
-    }
-
-    if (cartTotal.length !== 0) {
-        total = parseNumber(cartTotal.text()) - discount;
-    }
-
-    if (cartExtras.length !== 0) {
-        $.each(cartExtras, function (i, value) {
-            var parsed = parseNumber($(value).text());
-            if (parsed > 0) {
-                extras += parsed;
-            }
-        });
-    }
-
-    if (cartProductList.length !== 0) {
-        $.each(cartProductList, function (i, productElement) {
-            var obj = $(productElement);
-            var normalPrice = obj.find('.cart-product-list-item-price');
-            var offerPrice = obj.find('span.font-weight-bold');
-            var quantityElement = obj.find('.cart-product-list-item-quantity');
-
-            var quantity = 1;
-            if (quantityElement.length > 0) {
-                quantity = parseInt(quantityElement.text());
-            }
-
-            var price = null;
-
-            if (normalPrice.length > 0) {
-                price = normalPrice.text();
-            }
-            else if (offerPrice.length > 0) {
-                price = offerPrice.text();
-            }
-            if (price !== null) {
-                products.push({
-                    name: obj.find('.cart-product-list-item-name').text(),
-                    description: obj.find('.cart-product-list-item-description').text(),
-                    price: parseNumber(price) / quantity,
-                    quantity: quantity
-                });
-            }
-        });
-    }
-
-    return {
-        data: {
-            products: products,
-            total: total,
-            discount: discount,
-            extras: extras
-        }
-    };
-};
+injectScripts(['scripts/common.js', 'scripts/injectable/shop-info-export.js']);
--- Deploy/scripts/content/content-orders.js
+++ Deploy/scripts/content/content-orders.js
@@ -1,10 +1,19 @@
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.key === "popup_opened") {
-            var data = JSON.parse(localStorage.getItem(localStorageDataName));
-            sendDataToExtension('order_data_efood', data);
+            parseCartAndSend();
         }
     }
 );
 
-injectScripts(['scripts/common.js', 'scripts/injectable/cart-export.js']);
+function parseCartAndSend() {
+    var shopId = localStorage.getItem(localStorageShopId);
+    var localStorKey = `cart_new__${shopId}`;
+    var cart = JSON.parse(localStorage.getItem(localStorKey)) || {};
+    var parsedCart = parseCart(cart);
+    localStorage.setItem(localStorageDataName, JSON.stringify(parsedCart));
+
+    sendDataToExtension('order_data_efood', parsedCart);
+}
+
+injectScripts(['scripts/common.js', 'scripts/injectable/shop-info-export.js']);
